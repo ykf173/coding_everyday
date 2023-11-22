6b99ec51cfaad8d4e08f140a4cc7842b94c8f634
ykf173 <ykf_173@163.com>
2023-11-22 10:51:59 +0800

    更新常用模版

--- 常用模板/find/bisearch.py
+++ 常用模板/find/bisearch.py
@@ -1,3 +1,47 @@
+import random
+
+def quick_sorted(nums, left, right):
+    if left <= right:
+        low, high = left, right
+        cur_num = nums[left]
+        while low < high:
+            while low < high and nums[high] >= cur_num:
+                high -= 1
+            if low < high:
+                nums[low] = nums[high]
+            
+            while low < high and nums[low] <= cur_num:
+                low += 1
+            if low < high:
+                nums[high] = nums[low]
+        nums[low] = cur_num
+
+        quick_sorted(nums, left, low - 1)
+        quick_sorted(nums, low + 1, right)
+
+
 def bisearch(nums, target):
-    n = len(nums)
-    while i < n:
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid
+        else:
+            right = mid
+    return f'not found {target}'
+
+if __name__ == '__main__':
+    nums = [random.randint(1, 10000) for _ in range(1000)]
+    numsed = nums[:]
+    target = random.choice(nums)
+    quick_sorted(numsed, 0, len(numsed) - 1)
+    print(nums)
+    print(150*'-')
+    print(numsed)
+
+    print(set(nums) == set(numsed))
+    print(f'我要找{target}')
+    print(numsed.index(target))
+    print(bisearch(numsed, target))
--- 常用模板/sort/qs.py
+++ 常用模板/sort/qs.py
@@ -1,3 +1,31 @@
+import random
+
+
 def quick_sorted(nums, left, right):
-    if left < right:
-        
+    if left <= right:
+        low, high = left, right
+        cur_num = nums[left]
+        while low < high:
+            while low < high and nums[high] >= cur_num:
+                high -= 1
+            if low < high:
+                nums[low] = nums[high]
+            
+            while low < high and nums[low] <= cur_num:
+                low += 1
+            if low < high:
+                nums[high] = nums[low]
+        nums[low] = cur_num
+
+        quick_sorted(nums, left, low - 1)
+        quick_sorted(nums, low + 1, right)
+
+
+if __name__ == '__main__':
+    nums = [random.randint(1, 10000) for _ in range(1000)]
+    numsed = nums[:]
+    quick_sorted(nums, 0, len(nums) - 1)
+    print(numsed)
+    print(150*'-')
+    print(nums)
+    print(set(nums) == set(numsed))
